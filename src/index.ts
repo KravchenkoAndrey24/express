c25b1d693efa375d892cd8982dfadea485194a4b
Andrii Kravchenko <yazaandrey@gmail.com>
2024-01-21 21:46:26 +0100

    added index html file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public/index.html"));
+
 app.get("/", (req, res) => {
   res.json("Hello world!!!");
 });
