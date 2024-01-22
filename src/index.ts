c4e2ca105b74bbea85c0060cf9f4f432f7637491
Andrii Kravchenko <yazaandrey@gmail.com>
2024-01-22 13:03:31 +0100

    added vercel config

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public/index.html"));
-
 app.get("/", (req, res) => {
   res.json("Hello world!!!");
 });
