03d3b3b1398a54b7f28938bcd1372cd405313907
Andrii Kravchenko <yazaandrey@gmail.com>
2024-01-22 13:48:40 +0100

    fix vercel config

---
+++ src/api/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/api", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.json("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+module.exports = app;
