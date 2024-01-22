2dafa71606154032347882a713aea47f65afc6db
Andrii Kravchenko <yazaandrey@gmail.com>
2024-01-22 14:13:45 +0100

    set render config

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.json("Hello World");
