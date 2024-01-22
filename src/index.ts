dc7d6677f2740248ed50dc24cc337a1258a9c4d2
Andrii Kravchenko <yazaandrey@gmail.com>
2024-01-22 14:17:52 +0100

    test render deploy

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.json("Hello World");
+  res.json("Hello World!!!");
 });
 
 app.listen(port, () => {
