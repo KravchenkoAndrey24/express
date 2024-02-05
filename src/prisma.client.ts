5b34a6ef66238a486ebd46787aedfe43f97ffd1d
Andrii Kravchenko <yazaandrey@gmail.com>
2024-02-05 20:46:38 +0100

    fix: rebuild app

--- src/prisma.client.ts
+++ src/prisma.client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './../node_modules/@prisma/client';
 import dotenv from 'dotenv';
 
 export const connentToPrisma = async () => {
