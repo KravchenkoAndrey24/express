3467458e0e01bda1d01b08bfdaad08c4490fda09
Andrii Kravchenko <yazaandrey@gmail.com>
2024-02-16 15:54:41 +0100

    feat: remove express-session

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import { productRouter } from './domain/product/product.routes';
 import { authRouter } from './domain/auth/auth.routes';
 import AppDataSource from './typeOrm.config';
 import passport from 'passport';
-import session from 'express-session';
 
 export const runApp = async () => {
   const app = express();
@@ -13,10 +12,6 @@ export const runApp = async () => {
   const dbConnection = await AppDataSource.initialize();
 
   app.use(bodyParser.json());
-
-  if (process.env.NODE_ENV !== 'test') {
-    app.use(session({ secret: process.env.JWT_SECRET as string, resave: true, saveUninitialized: true }));
-  }
 
   app.use(passport.initialize());
 
